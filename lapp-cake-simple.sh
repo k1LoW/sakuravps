e14b2a12b1405f74aa2bfadfca8897900d4b7edf
k1LoW <k1lowxb@gmail.com>
2010-10-08 10:19:17 +0900

    Add lapp-cake-simple.sh

---
+++ lapp-cake-simple.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ -z $script_url ];then
+    export script_url="http://github.com/k1LoW/sakuravps/raw/master/"
+fi
+
+#common
+curl $script_url/common.sh|bash
+#iptables
+curl $script_url/iptables-simple.sh|bash
+/etc/init.d/iptables save
+#apache php
+curl $script_url/apache-php.sh|bash
+#postgresql
+curl $script_url/postgresql.sh|bash
