d1cdaf707ab6c641685f1d8421700d71f8bcdbd3
k1LoW <k1lowxb@gmail.com>
2010-10-08 10:18:07 +0900

    Update postgresql.sh

--- postgresql.sh
+++ postgresql.sh
@@ -4,9 +4,9 @@ rpm -ivh http://yum.pgsqlrpms.org/reporpms/8.4/pgdg-centos-8.4-2.noarch.rpm
 
 # yum install mysql
 yum -y install postgresql-tcl postgresql-server postgresql-contrib postgresql
-postgres -c "initdb --no-locale -D /var/lib/pgsql/data/ -E UNICODE"
-mkdir /var/log/postgres
-postgres -c "pg_ctl -D /var/lib/pgsql/data -l /var/log/postgres/logfile start"
+/etc/init.d/postgresql initdb --no-locale -D /var/lib/pgsql/data/ -E UNICODE
+# mkdir /var/log/postgres
+# postgres -c "pg_ctl -D /var/lib/pgsql/data -l /var/log/postgres/logfile start"
 
 /sbin/chkconfig postgresql on
 /etc/init.d/postgresql start
