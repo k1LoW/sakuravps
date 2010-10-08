1b8a322708059c734532068b5b2fc81024a9c3a9
k1LoW <k1lowxb@gmail.com>
2010-10-08 10:12:25 +0900

    Add postgresql.sh

---
+++ postgresql.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#postgresql yum repogitory
+rpm -ivh http://yum.pgsqlrpms.org/reporpms/8.4/pgdg-centos-8.4-2.noarch.rpm
+
+# yum install mysql
+yum -y install postgresql-tcl postgresql-server postgresql-contrib postgresql
+postgres -c "initdb --no-locale -D /var/lib/pgsql/data/ -E UNICODE"
+mkdir /var/log/postgres
+postgres -c "pg_ctl -D /var/lib/pgsql/data -l /var/log/postgres/logfile start"
+
+/sbin/chkconfig postgresql on
+/etc/init.d/postgresql start
