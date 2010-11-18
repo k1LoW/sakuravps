6df9ae6cd4d7d2ff63df66d3a576061e559ca08d
k1LoW <k1lowxb@gmail.com>
2010-11-18 10:54:11 +0900

    Fix typo

--- git.sh
+++ git.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 wget http://download.fedora.redhat.com/pub/epel/5/i386/epel-release-5-4.noarch.rpm
 rpm -ivh epel-release-5-4.noarch.rpm
+rm epel-release-5-4.noarch.rpm
 yum -y install git subversion-perl
-sed -i -e "s/enabled=1/enabled=0/" /etc/yums.repo.d/epel.repo
+sed -i -e "s/enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo
