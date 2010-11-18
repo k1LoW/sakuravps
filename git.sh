e3d2bac1547dc3501d34c933cc859feb5b07aaba
k1LoW <k1lowxb@gmail.com>
2010-11-18 11:46:02 +0900

    i386 > x86_64

--- git.sh
+++ git.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget http://download.fedora.redhat.com/pub/epel/5/i386/epel-release-5-4.noarch.rpm
+wget http://download.fedora.redhat.com/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm
 rpm -ivh epel-release-5-4.noarch.rpm
 rm epel-release-5-4.noarch.rpm
 sed -i -e "s/enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo
