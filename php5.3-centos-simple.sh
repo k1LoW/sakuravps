137b6001642746700d9936d4b745a2a7cab99853
k1LoW <k1lowxb@gmail.com>
2010-12-25 18:52:22 +0900

    Fix sh

--- php5.3-centos-simple.sh
+++ php5.3-centos-simple.sh
@@ -10,3 +10,16 @@ curl $script_url/iptables-simple.sh|bash
 /etc/init.d/iptables save
 #apache php
 curl $script_url/apache-php5.3.sh|bash
+#mysql
+yum -y install --enablerepo=remi mysql-server
+sed -i.org -e "s/\[mysqld\]/\[mysqld\]\ndefault-character-set = utf8/" /etc/my.cnf
+cat >> /etc/my.cnf <<EOF
+[client]
+default-character-set = utf8
+
+[mysqldump]
+default-character-set = utf8
+EOF
+
+/sbin/chkconfig mysqld on
+/etc/init.d/mysqld start
