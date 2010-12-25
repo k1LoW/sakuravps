3e8a50a9a47c81d283f88ab1518c17893e6d458d
k1LoW <k1lowxb@gmail.com>
2010-12-25 17:42:01 +0900

    Add sh

--- git.sh
+++ git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget http://download.fedora.redhat.com/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm
-rpm -ivh epel-release-5-4.noarch.rpm
+rpm -Uvh epel-release-5-4.noarch.rpm
 rm epel-release-5-4.noarch.rpm
 sed -i -e "s/enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo
 yum -y install git subversion-perl git-svn --enablerepo=epel
---
+++ php5.3-centos-simple.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -z $script_url ];then
+    export script_url="https://github.com/k1LoW/sakuravps/raw/master/"
+fi
+
+#common
+curl $script_url/common.sh|bash
+#iptables
+curl $script_url/iptables-simple.sh|bash
+/etc/init.d/iptables save
+#apache php
+curl $script_url/apache-php.sh|bash
