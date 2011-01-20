1d1a40d23de9411b0e0d5333715795f0d6ad8003
k1LoW <k1lowxb@gmail.com>
2011-01-20 14:53:43 +0900

    Add php5.3-lapp-cake-centos-simple.sh

---
+++ php5.3-lapp-cake-centos-simple.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+if [ -z $script_url ];then
+    export script_url="https://github.com/k1LoW/sakuravps/raw/master/"
+fi
+
+#common
+curl $script_url/common.sh|bash
+#iptables
+curl $script_url/iptables-simple.sh|bash
+/etc/init.d/iptables save
+#apache php
+curl $script_url/apache-php5.3.sh|bash
+
+#postgresql yum repogitory
+rpm -ivh http://yum.pgsqlrpms.org/reporpms/8.4/pgdg-centos-8.4-2.noarch.rpm
+
+# yum install mysql
+yum -y install postgresql-tcl postgresql-server postgresql-contrib postgresql
+/etc/init.d/postgresql initdb --no-locale -D /var/lib/pgsql/data/ -E UNICODE
+# mkdir /var/log/postgres
+# postgres -c "pg_ctl -D /var/lib/pgsql/data -l /var/log/postgres/logfile start"
+
+/sbin/chkconfig postgresql on
+/etc/init.d/postgresql start
