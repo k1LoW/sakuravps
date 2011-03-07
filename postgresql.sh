3d603353e5212d15d2b8546eecc79cb1fad9251b
k1LoW <k1lowxb@gmail.com>
2011-03-07 09:59:13 +0900

    change postgresql.sh

--- postgresql.sh
+++ postgresql.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-#postgresql yum repogitory
-rpm -ivh http://www.pgrpms.org/8.4/redhat/rhel-5-x86_64/postgresql-8.4.7-1PGDG.rhel5.x86_64.rpm
 
-# yum install mysql
-yum -y install postgresql-tcl postgresql-server postgresql-contrib postgresql
+# yum install postgresql
+yum -y install postgresql84-tcl postgresql84-server postgresql84-contrib postgresql84
 /etc/init.d/postgresql initdb --no-locale -D /var/lib/pgsql/data/ -E UNICODE
 # mkdir /var/log/postgres
 # postgres -c "pg_ctl -D /var/lib/pgsql/data -l /var/log/postgres/logfile start"
