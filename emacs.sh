cd6f9c872a0bc5807d56d7fc8266a82932af2556
k1LoW <k1lowxb@gmail.com>
2011-04-21 13:20:21 +0900

    Update emacs.sh

--- emacs.sh
+++ emacs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-yum install gcc
-wget ftp://ftp.ring.gr.jp/pub/GNU/emacs/emacs-23.2.tar.gz
-tar zxvf emacs-23.2.tar.gz
-cd emacs-23.2/
+yum -y install gcc
+wget ftp://ftp.ring.gr.jp/pub/GNU/emacs/emacs-23.3.tar.gz
+tar zxvf emacs-23.3.tar.gz
+cd emacs-23.3/
 ./configure --without-x
 make
 make install
