edc5f9811d1ec952edc322008f13df1659cae30b
k1LoW <k1lowxb@gmail.com>
2012-04-02 18:35:11 +0900

    Update Repository

--- apache-php5.3.sh
+++ apache-php5.3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # yum install httpd & php & mysql & postgresql
-wget http://download.fedora.redhat.com/pub/epel/5/i386/epel-release-5-4.noarch.rpm
+wget http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/5/x86_64/epel-release-5-4.noarch.rpm
 wget http://rpms.famillecollet.com/enterprise/remi-release-5.rpm
 rpm -Uvh remi-release-5*.rpm epel-release-5*.rpm
 yum -y install httpd
