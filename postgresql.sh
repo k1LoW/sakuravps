2e5583e4ec8a575a0c497c38f5ef3e49536a00e9
k1LoW <k1lowxb@gmail.com>
2012-08-10 14:23:44 +0900

    Update .sh

--- postgresql.sh
+++ postgresql.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # yum install postgresql
-yum -y install postgresql84-tcl postgresql84-server postgresql84-contrib postgresql84
+yum -y install postgresql postgresql-server postgresql-contrib postgresql-tcl
 /etc/init.d/postgresql initdb --no-locale -D /var/lib/pgsql/data/ -E UNICODE
-# mkdir /var/log/postgres
-# postgres -c "pg_ctl -D /var/lib/pgsql/data -l /var/log/postgres/logfile start"
 
 /sbin/chkconfig postgresql on
 /etc/init.d/postgresql start
