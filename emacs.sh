aea87efbe5ff8f3f2d1c7c4c7e77d4374f0bfdab
k1LoW <k1lowxb@gmail.com>
2012-10-15 16:08:20 +0900

    Update emacs.sh

--- emacs.sh
+++ emacs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-yum -y install gcc
-wget ftp://ftp.ring.gr.jp/pub/GNU/emacs/emacs-23.3.tar.gz
-tar zxvf emacs-23.3.tar.gz
-cd emacs-23.3/
+yum -y install gcc ncurses-devel
+wget ftp://ftp.ring.gr.jp/pub/GNU/emacs/emacs-23.4.tar.gz
+tar zxvf emacs-23.4.tar.gz
+cd emacs-23.4/
 ./configure --without-x
+make bootstrap
 make
 make install
