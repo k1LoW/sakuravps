7fb4ec28efeecf1a07ac296fe33c9fe4d59d26eb
k1LoW <k1lowxb@gmail.com>
2013-01-09 11:46:43 +0900

    remove default-character-set

--- mysql.sh
+++ mysql.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # yum install mysql
 yum -y install mysql mysql-server
-sed -i.org -e "s/\[mysqld\]/\[mysqld\]\ndefault-character-set = utf8/" /etc/my.cnf
+sed -i.org -e "s/\[mysqld\]/\[mysqld\]\ncharacter-set-server = utf8/" /etc/my.cnf
 cat >> /etc/my.cnf <<EOF
 [client]
-default-character-set = utf8
+character-set-server = utf8
 
 [mysqldump]
-default-character-set = utf8
+character-set-server = utf8
 EOF
 
 /sbin/chkconfig mysqld on
