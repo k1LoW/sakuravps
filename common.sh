d4304bca5fb8df82f132a60ff2cc1fd5d931f68d
k1LoW <k1lowxb@gmail.com>
2013-02-19 13:54:42 +0900

    modifiy common.sh

--- common.sh
+++ common.sh
@@ -1,41 +1,27 @@
 #!/bin/bash
-#yum
+# yum
 yum -y update
 
-#/etc/sysconfig/i18n
+# /etc/sysconfig/i18n
 sed -i.org -e "s/LANG=\"C\"/LANG=\"ja_JP.UTF-8\"/" /etc/sysconfig/i18n
 
-#stop services
+# stop services
 chkconfig acpid off
 chkconfig auditd off
-chkconfig autofs off
-chkconfig avahi-daemon off
-chkconfig bluetooth off
-chkconfig cups off
-chkconfig firstboot off
-chkconfig gpm off
 chkconfig haldaemon off
-chkconfig hidd off
-chkconfig kudzu off
 chkconfig lvm2-monitor off
-chkconfig mcstrans off
 chkconfig mdmonitor off
 chkconfig messagebus off
 chkconfig netfs off
-chkconfig nfslock off
-chkconfig pcscd off
-chkconfig portmap off
-chkconfig rawdevices off
 chkconfig restorecond off
-chkconfig rpcgssd off
-chkconfig rpcidmapd off
 chkconfig smartd off
-chkconfig xfs off
-chkconfig yum-updatesd off
 
-#stop ip6tables
+# stop ip6tables
 /etc/init.d/ip6tables stop
-/sbin/chkconfig ip6tables off
+chkconfig ip6tables off
+
+# yum install git
+yum -y install git
 
 # yum install subversion
 yum -y install subversion
