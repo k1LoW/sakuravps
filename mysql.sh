9de5d2acd090a34ef9f42cffad4c5658bb6c32e7
k1LoW <k1lowxb@gmail.com>
2013-02-19 14:12:43 +0900

    Add innodb_file_per_table

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # yum install mysql
 yum -y install mysql mysql-server
-sed -i.org -e "s/\[mysqld\]/\[mysqld\]\ncharacter-set-server = utf8\ndefault-storage-engine=innodb/" /etc/my.cnf
+sed -i.org -e "s/\[mysqld\]/\[mysqld\]\ncharacter-set-server = utf8\ndefault-storage-engine=innodb\ninnodb_file_per_table/" /etc/my.cnf
 cat >> /etc/my.cnf <<EOF
 
 [mysqldump]
