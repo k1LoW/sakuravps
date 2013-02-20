b03b064b0974de16cba27fd309c1594aaf7953ae
k1LoW <k1lowxb@gmail.com>
2013-02-20 15:18:49 +0900

    Update growthforecast.sh

--- growthforecast.sh
+++ growthforecast.sh
@@ -3,11 +3,18 @@
 yum groupinstall "Development Tools"
 yum -y install pkgconfig glib2-devel gettext libxml2-devel pango-devel cairo-devel
 
-useradd growthforecast
-passwd growthforecast
-
-su - growthforecast
-
 curl -kL http://install.perlbrew.pl | bash
 echo '[[ -s "$HOME/perl5/perlbrew/etc/bashrc" ]] && source "$HOME/perl5/perlbrew/etc/bashrc"' >> .bash_profile
 source $HOME/perl5/perlbrew/etc/bashrc
+
+perlbrew install perl-5.16.2
+perlbrew switch perl-5.16.2
+
+perlbrew install-cpanm
+
+cpanm -n GrowthForecast
+
+mkdir -p /var/growthforecast/data
+mkdir -p /var/growthforecast/log
+
+growthforecast.pl --port=80 --data-dir=/var/growthforecast/data > /var/growthforecast/log/growthforecast.log 2> /var/growthforecast/log/growthforecast.err &
