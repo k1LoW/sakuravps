556a8e3f677f2f8e1e90b94582ae7e3f2118c1c3
k1LoW <k1lowxb@gmail.com>
2013-02-20 11:05:08 +0900

    Add td-agent.sh

---
+++ td-agent.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# add repo
+cat <<EOF  > /etc/yum.repos.d/td.repo
+[treasuredata]
+name=TreasureData
+baseurl=http://packages.treasure-data.com/redhat/\$basearch
+gpgcheck=0
+EOF
+
+yum update
+yum -y install td-agent
+/etc/init.d/td-agent start
